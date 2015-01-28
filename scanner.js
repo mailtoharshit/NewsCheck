ba586a5094ae9168d61e6227706e8edd959a26cd
Harshit Pandey <mailtoharshit@gmail.com>
2015-01-27 23:41:20 -0800

    Scanner Commented

--- scanner.js
+++ scanner.js
@@ -1,6 +1,12 @@
-//Endpoint URL - http://timesofindia.indiatimes.com/feeds/newsfeed/-2128936835.cms?feedtype=sjson
+/*
+* Copyright (c) 2015.
+* Author- Harshit Pandey
+* Feed Reader Script - Class for Reading URL feed through crawler script
+*Endpoint URL - http://timesofindia.indiatimes.com/feeds/newsfeed/-2128936835.cms?feedtype=sjson
+*/
 
 
+//Hold temp data for third iteration
 var dataArray  = [];
 
 //Method to make Ajax call and push data into array
@@ -10,19 +16,18 @@ $.getJSON( timesAPI, {})
   .done(function( data ) 
   {$.each(data, function(key, value) {
        $.each(value, function(key, value){
-       dataArray.push(value);
+       dataArray.push(value);  //push third level nested value
   });
-
 })});}
 
+//Jscript doesnt hold and wait for context to finish, hence in chrome console run this command individually to test
 //populate array 
 times();
 //print array
 dataArray;
 
-//clean up array
+//clean up array - last part under contrustion, once this work, I can parse through url and dump on a page.
 for (i = 0; i < 4; i++) {dataArray.shift();}
-
 var stories = [];
 function scan(data){ 
   $.each(data, function(idx, obj){ 
