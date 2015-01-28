86c9a3186da73f61603da96b00e47b3fc36d7d10
Harshit Pandey <mailtoharshit@gmail.com>
2015-01-27 23:35:05 -0800

    Updated Enpoint url for documentation

--- scanner.js
+++ scanner.js
@@ -1,13 +1,17 @@
-//working
+//Endpoint URL - http://timesofindia.indiatimes.com/feeds/newsfeed/-2128936835.cms?feedtype=sjson
+
+
 var dataArray  = [];
+
+//Method to make Ajax call and push data into array
 function times(){
-  var timesAPI = "http://timesofindia.indiatimes.com/feeds/newsfeed/-2128936835.cms?feedtype=json";
-  $.getJSON( timesAPI, {})
-    .done(function( data ) 
-    {$.each(data, function(key, value) {
-         $.each(value, function(key, value){
-         dataArray.push(value);
-    });
+var timesAPI = "http://timesofindia.indiatimes.com/feeds/newsfeed/-2128936835.cms?feedtype=json";
+$.getJSON( timesAPI, {})
+  .done(function( data ) 
+  {$.each(data, function(key, value) {
+       $.each(value, function(key, value){
+       dataArray.push(value);
+  });
 
 })});}
 
@@ -20,12 +24,12 @@ dataArray;
 for (i = 0; i < 4; i++) {dataArray.shift();}
 
 var stories = [];
-  function scan(data){ 
-    $.each(data, function(idx, obj){ 
-        $.each(obj, function(key, value){ 
-            if(key.toString().toLowerCase() ==="story")
-            console.log(key + ": " + value);
-        });
-    }); }
+function scan(data){ 
+  $.each(data, function(idx, obj){ 
+      $.each(obj, function(key, value){ 
+          if(key.toString().toLowerCase() ==="story")
+          console.log(key + ": " + value);
+      });
+  }); }
 
 
