fc3a91d99c6b3dae77aa6109c2a19594d7c88c83
Harshit Pandey <mailtoharshit@gmail.com>
2015-01-27 23:32:33 -0800

    Added a scanner script- partially working inner loop is broken

---
+++ scanner.js
@@ -0,0 +1,31 @@
+//working
+var dataArray  = [];
+function times(){
+  var timesAPI = "http://timesofindia.indiatimes.com/feeds/newsfeed/-2128936835.cms?feedtype=json";
+  $.getJSON( timesAPI, {})
+    .done(function( data ) 
+    {$.each(data, function(key, value) {
+         $.each(value, function(key, value){
+         dataArray.push(value);
+    });
+
+})});}
+
+//populate array 
+times();
+//print array
+dataArray;
+
+//clean up array
+for (i = 0; i < 4; i++) {dataArray.shift();}
+
+var stories = [];
+  function scan(data){ 
+    $.each(data, function(idx, obj){ 
+        $.each(obj, function(key, value){ 
+            if(key.toString().toLowerCase() ==="story")
+            console.log(key + ": " + value);
+        });
+    }); }
+
+
