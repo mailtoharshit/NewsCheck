b3a649a5ecc159e2e3f20fe500beb3b50016feea
Harshit Pandey <mailtoharshit@gmail.com>
2015-01-28 12:05:55 -0800

    Added Code to handle text with key-value pair

--- scanner.js
+++ scanner.js
@@ -1,6 +1,3 @@
-//Endpoint URL - http://timesofindia.indiatimes.com/feeds/newsfeed/-2128936835.cms?feedtype=sjson
-
-
 var dataArray  = [];
 
 //Method to make Ajax call and push data into array
@@ -8,21 +5,18 @@ function times(){
 var timesAPI = "http://timesofindia.indiatimes.com/feeds/newsfeed/-2128936835.cms?feedtype=json";
 $.getJSON( timesAPI, {})
   .done(function( data ) 
-  {$.each(data, function(key, value) {
-       $.each(value, function(key, value){
-       dataArray.push(value);
-  });
-
+  {$.each(data['NewsItem'], function(key, value) {
+       if (value.Story.div.div['#text']) dataArray.push({url: value.WebURL, text: value.Story.div.div['#text'].join('')});  //push third level nested value
 })});}
 
+//Jscript doesnt hold and wait for context to finish, hence in chrome console run this command individually to test
 //populate array 
 times();
 //print array
 dataArray;
 
-//clean up array
+//clean up array - last part under contrustion, once this work, I can parse through url and dump on a page.
 for (i = 0; i < 4; i++) {dataArray.shift();}
-
 var stories = [];
 function scan(data){ 
   $.each(data, function(idx, obj){ 
@@ -31,5 +25,3 @@ function scan(data){
           console.log(key + ": " + value);
       });
   }); }
-
-
